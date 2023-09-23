a8c580d1716dbb9559ad0fbbef36f52ac28214d4
Scoges <scott.ogrins@gmail.com>
2023-09-23 13:01:44 -0300

    started tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,13 +3,42 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  await Tag.findAll({
+    attributes: ["id", "tag_name"],
+    include: [{
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      through: "ProductTag",
+    }, ],
+  })
+  .then((parsedTagData) => {
+    res.json(parsedTagData);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
+
+
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findByPk(req.params.id, {
+    include: [{
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      through: "ProductTag",
+    }],
+  })
+  .then((retrievedTag) => {
+    res.json(retrievedTag);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
   // be sure to include its associated Product data
 });
 
